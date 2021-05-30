ef6ebd9aec0033f8eb271eee2be627fdf3c0d95c
ozora <ozora.ogino.1127@gmail.com>
2021-05-30 15:52:36 +0900

    change thumbnail

--- src/data/config.js
+++ src/data/config.js
@@ -13,7 +13,7 @@ module.exports = {
     // youtube: 'https://www.youtube.com/',
     // google: 'https://plus.google.com/u/0/xxx',
   },
-  googleAnalyticsID: 'UA-88875900-4',
+  googleAnalyticsID: 'UA-85900-4',
   themeColor: '#6b63ff',
   backgroundColor: '#6b63ff',
   social: {
