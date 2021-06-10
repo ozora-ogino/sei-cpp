b8238ca0e44f7bd9e8a533853a01f49136f76629
ozora <ozora.ogino.1127@gmail.com>
2021-06-10 11:56:41 +0900

    Fix contents

--- src/components/landing/Merit/index.jsx
+++ src/components/landing/Merit/index.jsx
@@ -26,8 +26,9 @@ export const Merit = () => {
             </ul>
             <div style={{ margin: '1.8rem auto auto auto', textAlign: 'center' }}>
               <h3 style={{ display: 'inline-block', textAlign: 'left', lineHeight: '4rem', fontSize: '20px' }}>
-                1. カリフォルニアで働くプロフェッショナルとの交流
-                <br />2. 複数のコースから自社にあったコースを柔軟に選択することができる
+                1. 自社の文化に沿ったDXのできる人材を作り出す
+                2. カリフォルニアで働くプロフェッショナルとの交流
+                3. NASAとCalPolyの共同プログラムに参加できる
                 <br />3. 自社の文化に沿ったDXのできる人材を作り出す</h3>
             </div>
             {/* <Link to={'/contact'}><Button>お問い合わせ</Button></Link> */}
