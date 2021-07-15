9e989bac8c4390c071543fdad1cf4d56e5366a12
ozora <ozora.ogino.1127@gmail.com>
2021-07-15 09:24:17 +0900

    Add Jeff's sride

--- src/pages/seminars/online/index.js
+++ src/pages/seminars/online/index.js
@@ -4,6 +4,7 @@ import { Intro, Details } from 'components/seminar/online'
 import jimmy from 'assets/speakers-icon/jimmy.jpeg'
 // import jimmyPDFImg from 'assets/pdf-cover/jimmy-2021S.png'
 import jimmyPDF from 'assets/pdf/jimmy-2021-spring.pdf'
+import jeffPDF from 'assets/pdf/jeff-2021-spring.pdf'
 import oishi from 'assets/speakers-icon/oishi.png'
 import jeff from 'assets/speakers-icon/jeff.png'
 
@@ -11,8 +12,8 @@ import jeff from 'assets/speakers-icon/jeff.png'
 export default () => {
 	const detailItems = [
 		{ title: "コロナ下における働き方改革", name: "Jimmy Onishi", company: "Microsoft", personImg: jimmy, pdfImg: jimmyPDF },
+		{ title: "", name: "Jeff Stanford", company: "Drive mode", personImg: jeff, pdfImg: jeffPDF },
 		// { title: "", name: "Takeshi Oishi", company: "Google", personImg: oishi, pdfImg: jimmyPDF },
-		// { title: "", name: "Jeff Stanford", company: "Drive mode", personImg: jeff, pdfImg: jimmyPDF },
 	]
 
 	return (
