d9df109f700f2a224358d124aa2592afd3c677f0
ozora <ozora.ogino.1127@gmail.com>
2021-07-16 17:29:21 +0900

    Fix

--- src/components/landing/Thumbnail/index.jsx
+++ src/components/landing/Thumbnail/index.jsx
@@ -3,82 +3,9 @@ import { Link } from 'gatsby';
 import Fade from 'react-reveal/Fade';
 import { Button } from 'components/common';
 import styled from 'styled-components';
-// import top from 'assets/illustrations/topcover.png'
-// import nasacpp from 'assets/illustrations/cover.svg'
-import nasacpp from 'assets/illustrations/cover-white-letter.svg'
+import coverImg from 'assets/illustrations/cover-white-letter.svg'
 // import nasacpp from 'assets/illustrations/nasacpp.png'
 
-
-// const Cover = styled.div`
-// // background: var(--primary);
-// vertical-align: middle;
-// img {
-//   // background: var(--primary);
-//   // background: white;
-//   margin-bottom: 0;
-//   width: 100%;
-//   // width: 30%;
-// }
-
-//   .cov{
-//     position: relative;
-//   }
-
-//   .top-letter {
-//     height: 70%;
-//     width: 50%;
-//     position: absolute;
-//     left: 0;
-//     top: calc(20% - 25px);
-//     // text-align: center;
-//     font-weight: bold;
-//     text-align: left;
-//     @media (max-width: 580px) {
-//       display: none;
-//     }
-//     // color: white;
-//     color: black;
-//   }
-
-//   .top-letter button {
-//     margin-top: 0;
-//       @media (max-width: 960px) {
-//         font-size: 14px;
-//         }
-//     @media (max-width: 680px) {
-//       font-size: 8px;
-//     }
-
-//     @media (max-width: 480px) {
-//       font-size: 8px;
-//     }
-//   }
-
-//   .letter-h2 {
-//     margin: 4rem 0 2rem 2rem;
-//     h2 {
-//       text-align: left;
-//       font-size: 20pt;
-//       @media (max-width: 960px) {
-//         font-size: 14pt;
-//         }
-//       @media (max-width: 680px) {
-//         margin-bottom: .5rem;
-//         font-size: 10px;
-//         }
-//       }
-
-//     @media (max-width: 960px) {
-//       margin: 2rem 0 0rem 2rem;
-//     }
-//     @media (max-width: 680px) {
-//       margin: 2rem 0 1rem 2rem;
-//     }
-//     @media (max-width: 480px) {
-//       margin: 1rem 0 .5rem 2rem;
-//     }
-// }
-// `;
 
 
 const Cover = styled.div`
@@ -174,7 +101,7 @@ export const Thumbnail = () => {
             <Link to={'/contact'}><Button>お問い合わせ</Button></Link>
           </div>
         </Fade>
-        <img src={nasacpp} />
+        <img src={coverImg} />
         <div className='bottom-letter'>
           <div className='letter-h2'>
             <h2>我々はSilicon Valleyのプロフェッショナルと共に</h2>
