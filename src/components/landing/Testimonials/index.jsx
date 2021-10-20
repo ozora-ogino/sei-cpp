037d8ded5e0edc77f9b1ecb7e707274dded9a861
Ozora Ogino <63685461+ozora-ogino@users.noreply.github.com>
2021-10-20 14:30:29 +0900

    Refactoring (#5)

--- src/components/landing/Testimonials/index.jsx
+++ src/components/landing/Testimonials/index.jsx
@@ -1,35 +1,21 @@
-import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
+import React from 'react';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 import { Wrapper } from './styles';
 
-
-export const Testimonials = (props) => {
-	return (
-		<Wrapper>
-			<Carousel
-				showArrows={true}
-				infiniteLoop={true}
-				showThumbs={false}
-				showStatus={false}
-				autoPlay={true}
-				interval={6100}
-			>
-				{props.data.map((item) => {
-					return (
-						<div>
-							<div className='myCarousel'>
-								<img src={item.img} />
-								<h3>{item.name}</h3>
-								<h4>{item.company}</h4>
-								<p>{item.message}</p>
-							</div>
-
-						</div>
-					)
-
-				})}
-			</Carousel>
-		</Wrapper>
-	);
-}
+export const Testimonials = props => (
+  <Wrapper>
+    <Carousel showArrows infiniteLoop showThumbs={false} showStatus={false} autoPlay interval={6100}>
+      {props.data.map(item => (
+        <div>
+          <div className="myCarousel">
+            <img src={item.img} />
+            <h3>{item.name}</h3>
+            <h4>{item.company}</h4>
+            <p>{item.message}</p>
+          </div>
+        </div>
+      ))}
+    </Carousel>
+  </Wrapper>
+);
